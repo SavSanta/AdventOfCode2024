53bc30fbaeb02bfbff592f60399fb8376a9a0742
administrator <sav@home.win>
2024-12-05 13:13:59 -0500

    Rudimentary solve of Day03 Part 1

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -15,82 +15,56 @@ impl SolutionSilver<usize> for Day {
         const MAX_NUM_LEN: usize = 4;
         let input = input.as_bytes();
 
-        memmem::find_iter(input, b"mul(")
-            .map(|idx| {
-                let num1_start = idx + 4;
-                let Some(num1_len) = input
-                    [num1_start..(num1_start + MAX_NUM_LEN).min(input.len())]
-                    .iter()
-                    .position(|&a| a == b',')
-                else {
-                    return 0;
-                };
-                if num1_len > MAX_NUM_LEN {
-                    return 0;
-                }
-                let num1_slice = &input[num1_start..num1_start + num1_len];
-                if num1_slice.iter().any(|&b| !b.is_ascii_digit()) {
-                    return 0;
-                }
-
-                let num2_start = num1_start + num1_len + 1;
-                let Some(num2_len) = input
-                    [num2_start..(num2_start + MAX_NUM_LEN).min(input.len())]
-                    .iter()
-                    .position(|&a| a == b')')
-                else {
-                    return 0;
-                };
-                if num2_len > MAX_NUM_LEN {
-                    return 0;
-                }
-                let num2_slice = &input[num2_start..num2_start + num2_len];
-                if num2_slice.iter().any(|&b| !b.is_ascii_digit()) {
-                    return 0;
-                }
-
-                let num1 = fast_parse_int_from_bytes(num1_slice);
-                let num2 = fast_parse_int_from_bytes(num2_slice);
-
-                num1 * num2
-            })
-            .sum()
-    }
+        1 as usize
+    } // calculate_silver
 }
 
 impl SolutionGold<usize, usize> for Day {
     const INPUT_SAMPLE_GOLD: &'static str = include_str!("input_sample_gold.txt");
 
     fn calculate_gold(input: &str) -> usize {
-        let mut enable = true;
-        let regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)").unwrap();
-        regex
-            .captures_iter(input.as_bytes())
-            .map(|ca| {
-                match ca.get(0).unwrap().as_bytes()[2] {
-                    b'l' => {
-                        // mul
-                        enable as usize
-                            * fast_parse_int_from_bytes(ca.get(1).unwrap().as_bytes())
-                            * fast_parse_int_from_bytes(ca.get(2).unwrap().as_bytes())
-                    }
-                    b'(' => {
-                        // do
-                        enable = true;
-                        0
-                    }
-                    b'n' => {
-                        // don't
-                        enable = false;
-                        0
-                    }
-                    _ => {
-                        unreachable!()
-                    }
+        use regex::Regex;
+        use std::borrow::BorrowMut;               // in order to be able to borrow mutably without prelude
+
+        let mut total: u32 = 0;
+
+        // Using \d \w\ whatever tokens produces
+        // rust Unicode-aware Perl class not found (make sure the unicode-perl feature is enabled)
+        // Bug for more detail but ut's full of stuff: https://github.com/rust-lang/regex/issues/982
+        let re_pattern = Regex::new(r"mul\([0-9]{1,3},[0-9]+\)").unwrap();
+        let mut results = vec![];
+
+        results = re_pattern.find_iter(input).map(|mut m| 
+                                                            m.borrow_mut()
+                                                            .as_str()
+                                                            .replace("mul(", "")
+                                                            .replace(")", ""))
+                                                            .collect();
+
+        // This is actually unused but this was as me trying to be more idiomatic (if not unreadable) and do it as a one-liner
+        // Ask on StackO or Red about how to do this
+        let splits = results.iter().map(|m| m.split_once(",").into_iter().collect::<Vec<_>>());
+
+        
+        // LLM teaching me how to split it out the long way with a bunch of lets. This makes up for above
+        for item in results {
+            if let Some((left, right)) = item.split_once(",")
+                {
+                    if let (Ok(left_into_num), Ok(right_into_num)) = (left.parse::<u32>(), right.parse::<u32>())
+                        {
+                            total += left_into_num * right_into_num;
+                        }
                 }
-            })
-            .sum()
-    }
+        }
+     
+        // Multiplication table finished not return the values
+        total as usize
+
+        
+        // In a future review visit go back above and edit reddit to use lookahead/around to skip the step where I replace the parentheses.
+
+
+    } //calculate gold
 }
 
 #[test]
@@ -114,5 +88,5 @@ fn test_gold_sample() {
 #[test]
 fn test_gold_real() {
     let output = Day::calculate_gold(Day::INPUT_REAL);
-    assert_eq!(106921067, output);
+    assert_eq!(190604937, output);
 }
