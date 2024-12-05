7b41e17d5d40473e40726f79b719a8d8d0d6963f
administrator <sav@home.win>
2024-12-05 14:47:26 -0500

    Implement ugly solution for Gold/Part2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -36,7 +36,6 @@ impl SolutionSilver<usize> for Day {
         // Ask on StackO or Red about how to do this
         let splits = results.iter().map(|m| m.split_once(",").into_iter().collect::<Vec<_>>());
 
-        
         // LLM teaching me how to split it out the long way with a bunch of lets. This makes up for above
         for item in results {
             if let Some((left, right)) = item.split_once(",")
@@ -63,12 +62,43 @@ impl SolutionGold<usize, usize> for Day {
 
     fn calculate_gold(input: &str) -> usize {
         const MAX_NUM_LEN: usize = 4;
-        let input = input.as_bytes();
+
+        use regex::Regex;
+        use std::str::Matches;
+
+        let mut total: u32 = 0;
+        let mut enabled: bool = true;
 
 
+        let re_pattern = Regex::new(r"mul\([0-9]{1,3},[0-9]+\)|don\'t\(\)|do\(\)").unwrap();
+        let matches = re_pattern.find_iter(input);
+        for matched  in matches {
+            //println!(" Data was {:?}:" , matched);
 
+            match matched.as_str() {
+                
+                "don't()" => enabled = false,
+                "do()" => enabled = true,
+                // My very very very very nasty and ugly hard to read implementation to derive the values. But it does work!!
+                // Make this cleaner later. Maybe start with less parens
+                _ => { 
 
-        1 as usize
+                    println!("we was on some other time here {:?}",  matched.as_str()); 
+                        if let Some((left, right)) = matched.as_str().replace("mul(", "").replace(")", "").split_once(",")
+                        {
+                            if let (Ok(left_into_num), Ok(right_into_num)) = (left.parse::<u32>(), right.parse::<u32>())
+                                {
+                                    if enabled {
+                                        total += (left_into_num * right_into_num);
+                                    }
+                                
+                                }
+                        }
+                    }
+                    ,
+            }
+        }
+        total as usize
     } //calculate gold
 }
 
@@ -93,5 +123,5 @@ fn test_gold_sample() {
 #[test]
 fn test_gold_real() {
     let output = Day::calculate_gold(Day::INPUT_REAL);
-    assert_eq!(190604937, output);
+    assert_eq!(82857512, output);
 }
