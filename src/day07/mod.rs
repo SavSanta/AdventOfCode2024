c4c473c226a65de27f9379c2d61fefe86fe9a903
administrator <sav@home.win>
2024-12-13 12:26:06 -0500

    implement Day07 part2 solu using Josh Ackland model

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -3,11 +3,13 @@ use super::*;
 
 pub struct Day;
 
-// 5  = 2 + 2 + 1
+
 fn equation_valid(expectedresult: u64, result: u64, index: u64, nums: Vec<u64> ) -> bool {
     // Recursive function as seen in others in Java, Kotlin, and CSharp
-    // HyperNeutrino has a great more mathy but concise solver
-
+    // HyperNeutrino has a great more math-y but concise solver. 
+    // Some the from highlighted backtracking and/or DFS to solve.
+    // Someone else really clever mentioned using the properties of maths such as divisibility and others. To optomize.
+    // 5  = 2 + 2 + 1
     if result > expectedresult {
         return false;
     }
@@ -17,7 +19,7 @@ fn equation_valid(expectedresult: u64, result: u64, index: u64, nums: Vec<u64> )
         // If we're at the last index of the array then we have no more numbers
         // May need to minus by 1 depending on 0-base or not contributes a factor
 
-        // CSince we're on the last loop we vcan check the overall result 
+        // Since we're on the last loop we can check the overall result 
         if  result == expectedresult {
             return true; 
         }
@@ -35,6 +37,56 @@ fn equation_valid(expectedresult: u64, result: u64, index: u64, nums: Vec<u64> )
     let new_result = result * current_num;
     if new_result <= expectedresult {
         return equation_valid(expectedresult, new_result, index+1, nums.clone());
+    }
+
+    return false;
+}
+
+fn equation_valid2(expectedresult: u64, result: u64, index: u64, nums: Vec<u64> ) -> bool {
+    // Part 2 we have an additonal operation check which involves concatenation. This function adds it as a third test
+
+    if result > expectedresult {
+        return false;
+    }
+
+    if index >= nums.len() as u64 {
+        // If we're at the last index of the array then we have no more numbers
+        // May need to minus by 1 depending on 0-base or not contributes a factor
+
+        // Since we're on the last loop we can check the overall result 
+        if  result == expectedresult {
+            return true; 
+        }
+        return false
+    }
+
+
+    let current_num = nums[index as usize];
+
+    let new_result = result + current_num;
+    if new_result <= expectedresult {
+        if equation_valid2(expectedresult, new_result, index+1, nums.clone()) {
+            return true;
+        }
+    }
+
+    let new_result = result * current_num;
+    if new_result <= expectedresult {
+        if equation_valid2(expectedresult, new_result, index+1, nums.clone()) {
+            return true;
+        }
+    }
+    
+    //let prev_num: u64 = nums[(&index - 1) as usize];  <--- Failure here.
+    // There is a failure as well on using newresult as the var. The function must use the result as below
+    // Need to study the recursiveness of this understand how recursive shit mentally works.
+    // Study studt. Study!
+    let concat_num : u64 = (result.to_string() + &current_num.to_string()).parse().expect("Issues with concatentation");
+    if concat_num <= expectedresult
+    {
+        let is_correct = equation_valid2(expectedresult, concat_num, &index+1, nums.clone());
+        //println!(" Correctness test of prev_num {prev_num}, current_num {current_num} = concat_num {concat_num} ==> {is_correct}");
+        return is_correct;
     }
 
     return false;
@@ -78,49 +130,29 @@ impl SolutionGold<u64, u64> for Day {
     const INPUT_SAMPLE_GOLD: &'static str = include_str!("input_sample_gold.txt");
 
     fn calculate_gold(input: &str) -> u64 {
-        input
+        #[derive(Debug)]
+        struct Equation {
+            total : u128,
+            numbers : Vec<u64>,
+        }
+      
+        // Organize the data into 
+        let mut parsed_data: Vec<Equation> = Vec::new();
+        parsed_data = input
             .lines()
             .map(|line| {
-                let (out, rest) = line.split_once(':').unwrap();
-                let expected_result = fast_parse_int(out) as u64;
-                let rest = rest
-                    .trim()
-                    .split(" ")
-                    .map(fast_parse_int)
-                    .map(|a| a as u64)
-                    .collect::<Vec<_>>();
+                let (total, nums) = line.split_once(": ").expect("Things went wrong on processing");
+                let numbers = nums.split(" ").map(|num| num.parse::<u64>().expect("Err: Processing u64s")).collect();
+                Equation { total: total.parse::<u128>().unwrap(), numbers }
+            }).collect();
+    
+       let solu1= parsed_data.iter()
+                                        .filter(|x| equation_valid2(x.total as u64, 0, 0, x.numbers.clone()))
+                                        .collect::<Vec<_>>()
+                                        .into_iter()
+                                        .fold(0, |acc, x| acc + x.total);
 
-                for i in 0..(3usize.pow(rest.len() as u32)) {
-                    let mut i = i;
-                    let mut current_result = 0u64;
-                    for &num in &rest {
-                        // if number is already too large, we won't get the expected result with future operations
-                        if current_result >= expected_result {
-                            break;
-                        }
-
-                        let operation = i % 3;
-                        i /= 3;
-
-                        match operation {
-                            0 => current_result *= num,
-                            1 => current_result += num,
-                            // assume that the first number is never 0, as the exercise does not define it
-                            2 if current_result == 0 => break,
-                            2 => {
-                                current_result = 10u64.pow(num.ilog10() + 1) * current_result + num;
-                            }
-                            _ => unreachable!(),
-                        }
-                    }
-
-                    if current_result == expected_result {
-                        return expected_result;
-                    }
-                }
-                0
-            })
-            .sum()
+        solu1 as u64
     }
 }
 
